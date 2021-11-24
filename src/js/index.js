dc6cb6a4b6b290528b4cea0e6c6d2c9ec5ec0750
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-11-24 11:57:54 +0300

    fixed linter

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
 import '../style.css';
-const card = document.querySelector('.cards')
 
-const baseUrl = "https://api.tvmaze.com/search/shows?q=girls";
+const card = document.querySelector('.cards');
+
+const baseUrl = 'https://api.tvmaze.com/search/shows?q=girls';
 
 const reachData = async () => {
-    const getData = await fetch(baseUrl);
-    const content = await getData.json();
-    return content;
-}
+  const getData = await fetch(baseUrl);
+  const content = await getData.json();
+  return content;
+};
 
-reachData().then(data =>
-    data.map(
-        (e) => {
-            card.innerHTML += `
+reachData().then((data) => data.forEach(
+  (e) => {
+    card.innerHTML += `
            <div class="card">
            <img src="${e.show.image.medium}" alt="">
            <div class="name-icon">
@@ -21,8 +21,6 @@ reachData().then(data =>
            </div>
            <button>Comments</button>
            </div>
-            `
-        }
-    )
-)
-
+            `;
+  },
+));
