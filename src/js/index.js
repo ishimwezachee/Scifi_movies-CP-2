1a753c9a53cb7f5b696a015165463b17e568b8f4
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-11-24 12:56:40 +0300

    button

--- src/js/index.js
+++ src/js/index.js
@@ -5,26 +5,29 @@ const card = document.querySelector('.cards');
 const baseUrl = 'https://api.tvmaze.com/search/shows?q=girls';
 
 const reachData = async () => {
-  const getData = await fetch(baseUrl);
-  const content = await getData.json();
-  return content;
+    const getData = await fetch(baseUrl);
+    const content = await getData.json();
+    return content;
 };
-
 reachData().then((data) => data.forEach(
-  (e) => {
-    card.innerHTML += `
+    (e) => {
+        card.innerHTML += `
            <div class="card">
            <img src="${e.show.image.medium}" alt="">
            <div class="name-icon">
                <P class="name">${e.show.name}</P>
                <i class="far fa-heart"></i>
            </div>
-           <button id="${e.show.id}" onclick="getId(this.id)">Comments</button>
+           <button class="comments" id="${e.show.id}">Comments</button>
            </div>
             `;
-  },
+    },
 ));
 
-function getId(id){
-    console.log(id);
-}
+
+const commentBtn = document.getElementsByTagName('button');
+console.log(commentBtn);
+
+
+
+
