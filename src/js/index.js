392b1d387c3914959709ef4f66796915a9541ca1
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-11-24 11:20:27 +0300

    added js for data

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 import '../style.css';
+const card = document.querySelector('.card')
+
+const baseUrl = "https://api.tvmaze.com/search/shows?q=girls";
+
+const reachData = async () => {
+    const getData = await fetch(baseUrl);
+    const content = await getData.json();
+    return content;
+}
+
+reachData().then(data =>
+    data.map(
+        (e) => {
+            card.innerHTML += `
+            <img src="${e.show.image.medium}" alt="">
+            <div class="name-icon">
+                <P class="name">${e.show.name}</P>
+                <i class="far fa-heart"></i>
+            </div>
+            <button>Comments</button>
+            `
+        }
+    )
+)
+
